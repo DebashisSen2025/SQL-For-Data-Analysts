3ac0be3c1ce18bc9ae51f4b5d66c3e88ee6c7855
Debashis Sen <sen.debashis.sd@gmail.com>
2026-03-17 18:32:16 +0530

    Update select_where_orderby.sql

--- 01_Basics/select_where_orderby.sql
+++ 01_Basics/select_where_orderby.sql
@@ -1,1 +1,18 @@
+-- SELECT ALL DATA
+SELECT * FROM customers;
 
+-- SELECT SPECIFIC COLUMNS
+SELECT name, country FROM customers;
+
+-- WHERE CONDITION
+SELECT * FROM customers
+WHERE country = 'India';
+
+-- ORDER BY DESCENDING
+SELECT * FROM customers
+ORDER BY salary DESC;
+
+-- GROUP BY
+SELECT country, COUNT(*)
+FROM customers
+GROUP BY country;
